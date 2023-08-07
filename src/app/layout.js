95b8937bcb6c93ee95a33b497170d0d74e1865ed
Sam B <rb.sam@icloud.com>
2023-08-07 18:21:37 +0100

    added multiple launch images

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,10 +14,20 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link
+          href="/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
           rel="apple-touch-startup-image"
-          media="(device-width: 828px) and (device-height: 1792px) and (-webkit-device-pixel-ratio: 3)"
-          href="/launchimage.png"
-        ></link>
+        />
+        <link
+          href="/iphonexr_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/iphonexsmax_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
         <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
         <link
           rel="apple-touch-icon"
